50c01fa7d82f7a6efbb282c6f33519f815ac96c4
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-08 18:02:05 +0800

    不能有连词符，取消更名

--- Formula/nexttrace-enhanced.rb
+++ Formula/nexttrace-enhanced.rb
@@ -1,4 +1,4 @@
-class Nexttrace-enhanced < Formula
+class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage ""
     version "beta"
@@ -8,10 +8,10 @@ class Nexttrace-enhanced < Formula
     depends_on "go" => :build
   
     def install
-      system "go", "build","-o nexttrace-enhanced", *std_go_args(ldflags: "-s -w")
+      system "go", "build", *std_go_args(ldflags: "-s -w")
     end
   
     test do
-      assert_match "NextTrace-enhanced", shell_output("#{bin}/nexttrace-enhanced -V")
+      assert_match "NextTrace", shell_output("#{bin}/nexttrace -V")
     end
   end
