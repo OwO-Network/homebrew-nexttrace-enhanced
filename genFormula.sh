176ed98bf9c1ac68f157d403578a2b6e7cd57ca7
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-08 21:29:53 +0800

    fix some bugs

--- genFormula.sh
+++ genFormula.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 export version=$(curl -s https://api.github.com/repos/owo-network/nexttrace-enhanced/releases/latest | jq ".name")
-md5=$(curl -sL https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:-1}.tar.gz | md5sum | cut -f1 -d' ')
-url="https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:-1}.tar.gz"
+md5=\"$(curl -sL https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:-1}.tar.gz | md5sum | cut -f1 -d' ')\"
+url=\"https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:-1}.tar.gz\"
 cat >Formula/nexttrace.rb <<EOF
 class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
-    homepage ""
+    homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
     version ${version}
     url ${url}
     md5 ${md5}
