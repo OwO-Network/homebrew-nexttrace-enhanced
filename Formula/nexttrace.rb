1866b48f0ce8b3f0f396efc6e6f0008b1b4b4488
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-08 18:07:38 +0800

    测试更新

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,7 +1,7 @@
 class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage ""
-    version "beta"
+    version "beta1"
     url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
     license "GPL-3.0"
 
