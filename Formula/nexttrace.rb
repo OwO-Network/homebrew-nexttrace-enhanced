028e638d9a3a75dede29c070a24056e8b26eba2a
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-08 17:11:01 +0800

    init

---
+++ Formula/nexttrace.rb
@@ -0,0 +1,16 @@
+class Nexttrace < Formula
+    desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
+    homepage ""
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
+    license "GPL-3.0"
+
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
+  
+    test do
+      assert_match "NextTrace", shell_output("#{bin}/nexttrace -V")
+    end
+  end
