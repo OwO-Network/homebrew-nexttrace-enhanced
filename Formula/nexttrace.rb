f61313a3969a25702406fb70be4c5870840f407a
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-09 15:15:27 +0800

    fix bug

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -2,14 +2,14 @@ class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
     version "v0.1.7-beta.4"
-    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.7-beta.4".tar.gz"
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.7-beta.4.tar.gz"
     sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X \'github.com/xgadget-lab/nexttrace/printer.version=v0.1.7-beta.4\' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.7-beta.4' -s -w")
     end
   
     test do
