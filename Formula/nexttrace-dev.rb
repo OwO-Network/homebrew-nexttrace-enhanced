84b7a0fd646e958e96f5a5fb9c8b35064eaa3103
sjlleo <13616352+sjlleo@users.noreply.github.com>
2022-06-10 22:41:30 +0000

    autoPublishNewFormulaBot

--- Formula/nexttrace-dev.rb
+++ Formula/nexttrace-dev.rb
@@ -1,15 +1,15 @@
 class NexttraceDev < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "64ad783d"
+    version "5e8b7b2d"
     url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
-    sha256 "b64ad783df450a9026012ec5305aa17b6254f881a868fec14beaf8b0a2f34565"
+    sha256 "b5e8b7b2daa43b6a21b178c5723e3ea2cb84e67f998390f5736e6a880466c786"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=64ad783d' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=5e8b7b2d' -s -w")
     end
   
     test do
--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,15 +1,15 @@
 class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "v0.1.7-beta.7"
-    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.7-beta.7.tar.gz"
-    sha256 "156cb0f70f0137d4a34c5a532c59d968b7b144d1f1015b8223fb5e919a1b0fde"
+    version "v0.1.7-beta.8"
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.7-beta.8.tar.gz"
+    sha256 "dcfad4ec90cc7d6ce9e7ba28e79f7608aac505899e17c3f9515b11efd74a6585"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.7-beta.7' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.7-beta.8' -s -w")
     end
   
     test do
