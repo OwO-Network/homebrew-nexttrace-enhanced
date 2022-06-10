b8f2e9a64db6e7c463429217ce804113542fbf18
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-10 18:59:56 +0800

    增加dev通道

---
+++ Formula/nexttrace-dev.rb
@@ -0,0 +1,18 @@
+class Nexttrace_dev < Formula
+    desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
+    homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
+    version "74e1bb9c"
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
+    sha256 "d74e1bb9c63a414dd8cfa2a72c8b6e1f22a1cbd50c7048cef545f7992c27b1e8"
+    license "GPL-3.0"
+
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=74e1bb9c' -s -w")
+    end
+  
+    test do
+      assert_match "NextTrace", shell_output("#{bin}/nexttrace -V")
+    end
+  end
--- genFormula.sh
+++ genFormula.sh
@@ -1,20 +1,43 @@
 # !/bin/bash
-export version=$(curl -s https://api.github.com/repos/owo-network/nexttrace-enhanced/releases/latest | jq ".name")
-sha256=\"$(curl -sL https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:$((${#version} - 1 - 1))}.tar.gz | sha256sum | cut -f1 -d' ')\"
-url=\"https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:$((${#version} - 1 - 1))}.tar.gz\"
+export version="$(curl -s https://api.github.com/repos/owo-network/nexttrace-enhanced/releases/latest | jq ".name")"
+url="https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/${version:1:$((${#version} - 1 - 1))}.tar.gz"
+sha256="$(curl -sL ${url} | sha256sum | cut -f1 -d' ')"
 cat >Formula/nexttrace.rb <<EOF
 class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
     version ${version}
-    url ${url}
-    sha256 ${sha256}
+    url "${url}"
+    sha256 "${sha256}"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
       system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=${version:1:$((${#version} - 1 - 1))}' -s -w")
+    end
+  
+    test do
+      assert_match "NextTrace", shell_output("#{bin}/nexttrace -V")
+    end
+  end
+EOF
+url="https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
+sha256="$(curl -sL ${url} | sha256sum | cut -f1 -d' ')"
+version_withoutquo=${sha256:1:8}
+cat >Formula/nexttrace-dev.rb <<EOF
+class Nexttrace_dev < Formula
+    desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
+    homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
+    version "${version_withoutquo}"
+    url "${url}"
+    sha256 "${sha256}"
+    license "GPL-3.0"
+
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=${version_withoutquo}' -s -w")
     end
   
     test do
