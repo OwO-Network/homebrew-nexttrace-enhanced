aa7b2e54104fbb66725cd6609f2eb99183ffd019
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-06-10 19:03:41 +0800

    修正类名错误

--- Formula/nexttrace-dev.rb
+++ Formula/nexttrace-dev.rb
@@ -1,4 +1,4 @@
-class Nexttrace_dev < Formula
+class NexttraceDev < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
     version "74e1bb9c"
