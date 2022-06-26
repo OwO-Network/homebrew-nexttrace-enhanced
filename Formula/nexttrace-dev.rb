b8b71bc522c2d67ac5cbf825dd8ef07e8efc3c5b
sjlleo <13616352+sjlleo@users.noreply.github.com>
2022-06-26 13:24:01 +0000

    autoPublishNewFormulaBot

--- Formula/nexttrace-dev.rb
+++ Formula/nexttrace-dev.rb
@@ -1,7 +1,7 @@
 class NexttraceDev < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "1656249467"
+    version "1656249841"
     url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
     sha256 "09cabb4cffa2f8e53f671750bdc2b964aa5b4a81c036c418cadea0ec0f514516"
     license "GPL-3.0"
@@ -9,7 +9,7 @@ class NexttraceDev < Formula
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=1656249467' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=1656249841' -s -w")
     end
   
     test do
--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,15 +1,15 @@
 class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "v0.1.10-beta.1"
-    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.10-beta.1.tar.gz"
-    sha256 "bb32189cf4b2b20d817959835cf25d06bd76f7928857bf788611625182d8d708"
+    version "v0.1.10-beta.2"
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.10-beta.2.tar.gz"
+    sha256 "f3fa511f821fd7ae32d855b18b3846adebf88e40dcc2e1b6619c1ebb0d1a9445"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.10-beta.1' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.10-beta.2' -s -w")
     end
   
     test do
