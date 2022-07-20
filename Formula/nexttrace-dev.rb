ff35eaaab8e203a7eb67fe4f88f62185fb799f19
sjlleo <13616352+sjlleo@users.noreply.github.com>
2022-07-20 06:02:42 +0000

    autoPublishNewFormulaBot

--- Formula/nexttrace-dev.rb
+++ Formula/nexttrace-dev.rb
@@ -1,15 +1,15 @@
 class NexttraceDev < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "1658296167"
+    version "1658296962"
     url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
-    sha256 "bb07b82345a342c047b71453f653e70a1c694b67ce23f2890722adc3f4feb1d1"
+    sha256 "40e78509728aff6236312c375be2695cd8337ff740d390f0501964c7592d15b8"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=1658296167' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=1658296962' -s -w")
     end
   
     test do
--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,15 +1,15 @@
 class Nexttrace < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "v0.1.11-beta.1"
-    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.11-beta.1.tar.gz"
-    sha256 "a2b6e697f95102663f51aabaae9f8dd361197b91bdaa0113c73b23f36b531ce4"
+    version "v0.1.11-beta.2"
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.1.11-beta.2.tar.gz"
+    sha256 "ba29bbec41022f64a2a3ec7328d31c203db854b06b0d5e33865c80a32b4302aa"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.11-beta.1' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.11-beta.2' -s -w")
     end
   
     test do
