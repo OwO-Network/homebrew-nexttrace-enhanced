f80932085395b32fbfe5aa1e628ca41da85977ee
sjlleo <13616352+sjlleo@users.noreply.github.com>
2022-08-20 12:26:47 +0000

    autoPublishNewFormulaBot

--- Formula/nexttrace-dev.rb
+++ Formula/nexttrace-dev.rb
@@ -1,15 +1,15 @@
 class NexttraceDev < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "1660960090"
+    version "1660998407"
     url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
-    sha256 "0b021e0d29471fc35b4b13b77a0f1c39f2ed32aaa0f04ac7f2247a948f6fcd85"
+    sha256 "d000ad917de9cdc4ffd8ac694bf07b408fce0ed8895607e77b68ea282cefd3c9"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=1660960090' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=1660998407' -s -w")
     end
   
     test do
