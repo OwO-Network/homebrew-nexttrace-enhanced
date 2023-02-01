9810be15ca6e49927bf990b9d324c8318511d5b3
sjlleo <13616352+sjlleo@users.noreply.github.com>
2023-02-01 04:47:08 +0000

    autoPublishNewFormulaBot

--- Formula/nexttrace-dev.rb
+++ Formula/nexttrace-dev.rb
@@ -1,14 +1,14 @@
 class NexttraceDev < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。\nDev通道"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "1675224620"
+    version "1675226828"
     url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/heads/main.zip"
-    sha256 "35ed026b396ee9f90ca3528689c61ed411f84296202b24eefc0bc4d1c6bc268b"
+    sha256 "db8567f9c034a0b9dbef3705796c552e640d900c11036f18e92d18f171eb26a8"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/OwO-Network/nexttrace-enhanced/printer.version=1675224620' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/OwO-Network/nexttrace-enhanced/printer.version=1675226828' -s -w")
     end
   end
--- Formula/nexttrace-enhanced.rb
+++ Formula/nexttrace-enhanced.rb
@@ -1,14 +1,14 @@
 class NexttraceEnhanced < Formula
     desc "一款开源的可视化路由跟踪工具，使用 Golang 开发。\n这是NextTrace加强版，旨在提供高度可定制化的可视化 Traceroute 工具。"
     homepage "https://github.com/OwO-Network/nexttrace-enhanced/"
-    version "v0.3.0-beta.3"
-    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.3.0-beta.3.tar.gz"
-    sha256 "45f1e37058dec820722f6dd7db2eb0254eceeb5602f6faa91a9ce4fbe0c551a6"
+    version "v0.3.1-beta.1"
+    url "https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v0.3.1-beta.1.tar.gz"
+    sha256 "e9f557933a63004d1e3a835aa80052a03cb3b02cafb2cd7e4b5f59c3e6321f9f"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/OwO-Network/nexttrace-enhanced/printer.version=v0.3.0-beta.3' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/OwO-Network/nexttrace-enhanced/printer.version=v0.3.1-beta.1' -s -w")
     end
   end
